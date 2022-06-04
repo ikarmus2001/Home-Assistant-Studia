d99e74f94e261cd48bbccc06d6e30481ffb84fc4
Kacper Hałaczkiewicz <54312917+ikarmus2001@users.noreply.github.com>
2022-06-04 19:27:37 +0200

    Final version

---
+++ secrets.h
@@ -0,0 +1,7 @@
+#ifndef MY_SECRETS
+#define MY_SECRETS
+
+extern const char* local_ssid;
+extern const char* local_password;
+
+#endif
---
+++ secrets_template.cpp
@@ -0,0 +1,7 @@
+#include "secrets.h"
+
+// Just rename to 'secrets.cpp' and pass your credentials
+
+// YOUR CREDENTIALS
+const char* ssid = "<YOUR SSID>";
+const char* password = "<YOUR PASSWORD>";
